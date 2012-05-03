dfefb1a51e6efbf4c093ee89f5ced40ee0d65042
Vasili Bulkin <bulkin@bt.b>
2012-05-03 19:22:07 +0400

    minor cleanup

--- cpp-srv/yafcd.cpp
+++ cpp-srv/yafcd.cpp
@@ -8,21 +8,6 @@
 using namespace std;
 using namespace boost;
 
-void test_ptr(asio::io_service &io_service)
-{
-	UserList::user_ptr user(new ChatUser(io_service,
-	                                     [](string a, string b)
-	                                     { cout << a << b << endl; }));
-	UserList users;
-	users.add(user)->disconnect();
-	users.add(UserList::user_ptr(new ChatUser(io_service,
-	                                          [](string a, string b)
-	                                          { cout << b << a << endl; })));
-	users.cleanup();
-	cout << users.count() << endl;
-}
-
-
 int main()
 {
 	int arr[] = { 1, 2, 3, 4, 5 };
